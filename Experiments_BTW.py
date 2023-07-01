ab645d9d62378b06e75377b69f02db67da446c66
konstantinoskevopoulos <kevopoulos.kon@gmail.com>
2023-07-01 15:09:32 +0200

    commented code and added some experiments

--- Experiments_BTW.py
+++ Experiments_BTW.py
@@ -52,12 +52,12 @@ def bins_count(bin_size, spikes):
         group = spikes[i:i + bin_size]  # Slice the list to get the current group of elements
         group_sum = sum(group)  # Sum the elements in the current group
         avalanches_pot.append(group_sum)  # Append the group sum to the result list
-
+    # do not take into account the avalanches of size 0.
     f = lambda x: x == 0
     aval_proper = [i for k, g in groupby(avalanches_pot, f) for i in (g if k else (sum(g),))]
 
     branch = []
-
+    # calculate the branching parameter
     for i in range(0, len(avalanches_pot) - 1):
         group1 = avalanches_pot[i:i + 2]  # Slice the list to get the current group of elements
         if group1[1] != 0:
@@ -84,7 +84,7 @@ def animate(k, u):
 # anim = animation.FuncAnimation(plt.figure(), animate, interval=1, frames=frames, repeat=False)
 # anim.save("spike_heat.gif")
 
-def power_law_dist(size, configs, frames, subsample_sort=0):
+def power_law_dist(size, configs, frames, subsample_sort=0, refr=False, inh=False):
     """
     Function that runs the simulation of the CA, plots the number of spikes per frame
     and the distribution of avalanche sizes. The function also performs a statistical test,
@@ -94,12 +94,19 @@ def power_law_dist(size, configs, frames, subsample_sort=0):
     configs: a tuple, that determines the configuration of "alpha" and "h" parameters of the simulation
     frames: the number of time frames (time steps) that will be performed in the simulation
     size: the size of the square grid. Will be used to demonstrate the robustness of criticality
+    subsample_sort: the choice of subsampling strategy to be followed. In all our simulations, 100 neurons are randomly
+    chosen and they are sampled out of the 50x50 grid.
+    refr: boolean variable. When it is True the experiment assuming refractory period between the spiking of neurons is
+    being performed.
+    inh: boolean variable. When it is True the experiment assuming inhibitory connections between neurons is
+    being performed.
 
     Output:
     The function produces the aforementioned plots and plots the results of the statistical tests
     """
     for config in configs:
-        u = BTW(size, 0, frames + 500, config[1], 'Param', config[0], subsample_sort)
+        # run the actual simulation
+        u = BTW(size, 0, frames + 500, config[1], 'Param', config[0], subsample_sort, refractory=refr, inhibitory=inh)
 
         # plot the spikes
         time = u[0][10:]
@@ -113,8 +120,9 @@ def power_law_dist(size, configs, frames, subsample_sort=0):
         bins = [1, 2, 4, 8, 16, 32]
         avalanches_per_bin = {}
         for binn in bins:
+            # compute the avalanches per bin
             avalanches_per_bin[binn] = bins_count(bin_size=binn, spikes=spikes_num)[0]
-
+            # compute the f(s), frequency of avalanches for different sizes
             frequency = Counter(avalanches_per_bin[binn])
             frequency = dict(sorted(frequency.items()))
             values, counts = zip(*frequency.items())
@@ -139,7 +147,8 @@ def power_law_dist(size, configs, frames, subsample_sort=0):
         plt.show()
 
 
-def branching_param_plot(configs, frames, subsample_sort=0):
+def branching_param_plot(configs, frames, subsample_sort=0, refr=False, inh=False, mult_runs=False, configs_mult=None,
+                         subsample_sort_mult=0):
     """
     Function that runs the simulation of the CA and plots the branching parameter per bin,
     for all the different parameter configurations given by the user.
@@ -150,35 +159,64 @@ def branching_param_plot(configs, frames, subsample_sort=0):
     Input:
     configs: a tuple, that determines the configuration of "alpha" and "h" parameters of the simulation
     frames: the number of time frames (time steps) that will be performed in the simulation
+    subsample_sort: the choice of subsampling strategy to be followed. In all our simulations, 100 neurons are randomly
+    chosen and they are sampled out of the 50x50 grid.
+    refr: boolean variable. When it is True the experiment assuming refractory period between the spiking of neurons is
+    being performed.
+    inh: boolean variable. When it is True the experiment assuming inhibitory connections between neurons is
+    being performed.
+    mult_runs: boolean variable. When it is True, we simulate the system multiple times and compute an estimate
+    for the branching parameter.
+    configs_mult: the parameter configurations for the above experiment
+    subsample_sort_mult: the choice of subsampling strategy to be followed. In all our simulations, 100 neurons are randomly
+    chosen and they are sampled out of the 50x50 grid. again this refers to the above experiment
 
     Output:
     store_data: This is a dictionary that stores the simulation data for each different parameter configuration.
     These data will be then used in the next experiment, to avoid simulating the system again.
+    branches_per_bin: depending on the experiment, the function could also return a dictionary that contains the branching
+    parameter per bin.
     """
-    store_data = {}
-    plt.figure(figsize=(10, 6))
-    for config in configs:
-        u = BTW(50, 0, frames + 500, config[1], 'Param', config[0], subsample_sort)
-        store_data[config] = u
+    if not mult_runs:
+        store_data = {}
+        plt.figure(figsize=(10, 6))
+        for config in configs:
+            u = BTW(50, 0, frames + 500, config[1], 'Param', config[0], subsample_sort, refractory=refr, inhibitory=inh)
+            store_data[config] = u
+            spikes_num = u[1][10:]
+            bins = [1, 2, 4, 8, 16, 32]
+            branches_per_bin = {}
+            for binn in bins:
+                # again compute the branching parameter per bin and per parameter configuration
+                branches_per_bin[binn] = bins_count(bin_size=binn, spikes=spikes_num)[1]
+            # visualise the branching parameters
+            print(branches_per_bin.items())
+            values_branch, counts_branch = zip(*branches_per_bin.items())
+            plt.semilogx(values_branch, counts_branch, marker='o', linestyle='-',
+                         label=f' alpha={config[0]}, h={config[1]}')
+            plt.axhline(1, color='black', linestyle='--')
+            plt.grid(True)
+            plt.legend(loc='best')
+            plt.xlabel('Bin size(ms)')
+            plt.ylabel(r'$\sigma$, Branching parameter ')
+            plt.title(r'Estimated $\sigma$, changed with bin size')
+        plt.show()
+
+        return store_data
+
+    if mult_runs:
+        u = BTW(50, 0, frames + 500, configs_mult[1], 'Param', configs_mult[0],
+                subsample_sort_mult, refractory=refr, inhibitory=inh)
         spikes_num = u[1][10:]
         bins = [1, 2, 4, 8, 16, 32]
         branches_per_bin = {}
         for binn in bins:
+            # again compute the branching parameter per bin and per parameter configuration
             branches_per_bin[binn] = bins_count(bin_size=binn, spikes=spikes_num)[1]
 
-        print(branches_per_bin.items())
-        values_branch, counts_branch = zip(*branches_per_bin.items())
-        plt.semilogx(values_branch, counts_branch, marker='o', linestyle='-',
-                     label=f' alpha={config[0]}, h={config[1]}')
-        plt.axhline(1, color='black', linestyle='--')
-        plt.grid(True)
-        plt.legend(loc='best')
-        plt.xlabel('Bin size(ms)')
-        plt.ylabel(r'$\sigma$, Branching parameter ')
-        plt.title(r'Estimated $\sigma$, changed with bin size')
-    plt.show()
+        _, counts_branch_mult = zip(*branches_per_bin.items())
 
-    return store_data
+        return counts_branch_mult
 
 
 def new_avalanche_measures(configs, stored_data):
@@ -191,13 +229,14 @@ def new_avalanche_measures(configs, stored_data):
     configs: a tuple, that determines the configuration of "alpha" and "h" parameters of the simulation
     """
     for config in configs:
+        # no need to run the simulation again, same parameter configurations with the above experiment
         u = stored_data[config]
         spikes_num = u[1][10:]
         bins = [1, 2, 4, 8, 16, 32]
         avalanches_per_bin = {}
         for binn in bins:
             avalanches_per_bin[binn] = bins_count(bin_size=binn, spikes=spikes_num)[0]
-
+        # compute the f(s=1,bs) and visualise it
         s1 = []
         for binn in bins:
             s1.append(Counter(avalanches_per_bin[binn])[1])
@@ -210,6 +249,7 @@ def new_avalanche_measures(configs, stored_data):
         plt.title(f'A different avalanche measure to assess criticality')
     plt.show()
 
+    # visualisation of the mean avalanche size per bin and per parameter configuration
     for config in configs:
         u = stored_data[config]
         spikes_num = u[1][10:]
@@ -219,7 +259,7 @@ def new_avalanche_measures(configs, stored_data):
         for binn in bins:
             avalanches_per_bin[binn] = bins_count(bin_size=binn, spikes=spikes_num)[0]
             mean_aval_size.append(np.mean(avalanches_per_bin[binn]))
-
+        # plot of the mean avalanche size
         plt.loglog(bins, mean_aval_size, marker='o', linestyle='-', label=f'alpha={config[0]}, h={config[1]}')
         plt.xlabel('Bin size(ms)')
         plt.grid(True)
@@ -229,20 +269,65 @@ def new_avalanche_measures(configs, stored_data):
     plt.show()
 
 
-
 power_law_configs = [(1, 9.25e-8), (1, 1e-6), (0.95, 1e-4), (0, 8e-4)]
-measures_branch_configs = [(1, 9.25e-8), (0.9, 0.0008), (0, 0.025), (0.99, 0.000002), (0.999, 0.000004), (1, 0.000001), (0.98, 0.000009)]
+measures_branch_configs = [(1, 9.25e-8), (0.9, 0.0008), (0, 0.025), (0.99, 0.000002), (0.999, 0.000004), (1, 0.000001),
+                           (0.98, 0.000009)]
+
 
 # actual experiments and plots without subsampling
-# power_law_dist(size=50, frames=52500, configs=power_law_configs)
-# stor_data = branching_param_plot(configs=measures_branch_configs, frames=52500)
-# new_avalanche_measures(configs=measures_branch_configs, stored_data=stor_data)
+power_law_dist(size=50, frames=52500, configs=power_law_configs)
+stor_data = branching_param_plot(configs=measures_branch_configs, frames=49500)
+new_avalanche_measures(configs=measures_branch_configs, stored_data=stor_data)
+
+grid_sizes = [40, 60]
+for size_grid in grid_sizes:
+    power_law_dist(size=size_grid, frames=99500, configs=power_law_configs)
+
+# actual experiments and plots with subsampling
+power_law_dist(size=50, frames=52500, configs=power_law_configs, subsample_sort=1)
+stor_data_100 = branching_param_plot(configs=measures_branch_configs, frames=52500, subsample_sort=1)
+new_avalanche_measures(configs=measures_branch_configs, stored_data=stor_data_100)
+
+# experiments with the refractory period of neurons
+power_law_dist(size=50, frames=14500, configs=power_law_configs, refr=True)
+stor_data_refr = branching_param_plot(configs=measures_branch_configs, frames=14500, refr=True)
+new_avalanche_measures(configs=measures_branch_configs, stored_data=stor_data_refr)
+
+# experiments with inhibitory connections of neurons
+power_law_dist(size=50, frames=14500, configs=power_law_configs, inh=True)
+stor_data_inh = branching_param_plot(configs=measures_branch_configs, frames=14500, inh=True)
+new_avalanche_measures(configs=measures_branch_configs, stored_data=stor_data_inh)
 
 
-#grid_sizes = [70]
-#for size_grid in grid_sizes:
-#    power_law_dist(size=size_grid, frames=52500, configs=power_law_configs)
+def branching_param_mult(subsample_choice):
+    # Estimate the branching parameter for a specific parameter configration
+    mult_branch_configs = [(0.9, 0.0008), (0.999, 4e-6)]
+    bins = [1, 2, 4, 8, 16, 32]
+    plt.figure()
+    # for the different configurations of the parameters
+    for param in mult_branch_configs:
+        calc_dict = {}
+        for i in range(15):
+            # calculate the branching parameter, means, st.devs and confidence intervls
+            calc_dict[i] = branching_param_plot(configs=param, frames=2500, mult_runs=True, configs_mult=param,
+                                                subsample_sort_mult=subsample_choice)
 
-# actual experiments and plots
-power_law_dist(size=50,  frames=52500, configs=power_law_configs[1:], subsample_sort = 1)
-#stor_data_100 = branching_param_plot(configs=measures_branch_configs, frames=52500, subsample_sort = 1)
+        transposed_lists = np.transpose(list(calc_dict.values()))
+        means = [np.mean(lst) for lst in transposed_lists]
+        confidence_intervals = [stats.t.interval(0.95, len(lst) - 1, loc=np.mean(lst), scale=stats.sem(lst)) for lst in
+                                transposed_lists]
+        # plot the above calculated measures
+        plt.semilogx(bins, means, 'o-', label=f'alpha={param[0]}, h={param[1]}')
+        plt.fill_between(bins, [ci[0] for ci in confidence_intervals], [ci[1] for ci in confidence_intervals],
+                         alpha=0.3, label='95% Confidence Interval')
+        plt.xlabel('Bin size (ms)')
+        plt.ylabel(r"$\sigma$, branching parameter")
+        plt.title(r"Estimated $\sigma$, changed with bin size")
+        plt.axhline(1, color='black', linestyle='--')
+        plt.legend()
+    plt.show()
+
+
+subsample_sort = [0, 1]
+for subsample_choice in subsample_sort:
+    branching_param_mult(subsample_choice)
--- Neuron_data_analysis.py
+++ Neuron_data_analysis.py
@@ -3,11 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 from tqdm import tqdm
+from scipy import stats
 from collections import Counter
+from itertools import groupby
 
+# load the in vivo data in the memory
 df = pd.read_csv("Data_neuron.txt", names=['spike times'], sep=" ")
 in_vivo = df['spike times'][:9500]
 
+# plot the spikes of the neuron in time
 plt.bar(in_vivo, np.ones(len(in_vivo)))
 plt.xlabel('ms')
 plt.ylabel('Spikes of neuron')
@@ -17,6 +21,20 @@ time_line = np.arange(0, math.ceil(in_vivo[len(in_vivo)-1]))
 
 
 def count_avalanches(bin_size, time_line, in_vivo):
+    """
+    Function that counts the avalanches occurring in a bin, given the different bin size.
+    The function also calculates the branching parameter for the given bin size.
+
+    Input:
+    bin_size: the desired bin size, given by the user. This determines how many timesteps are included in one bin.
+    time_line: this a list that determines the time frame of the simulation.
+    in_vivo: this is a list containing the spiking times of the 1 neuron in the in-vivo data.
+
+    Output:
+    aval_proper: this is a list that contains the number of avalanches occurring in each bin.
+    branching: this is a list that contains the estimated branching parameter for every bin.
+    """
+    # determine the time interals created, according to the given bin size
     num_intervals = math.ceil(len(time_line) / bin_size)
     avalanches = []
     orig = 0
@@ -24,13 +42,15 @@ def count_avalanches(bin_size, time_line, in_vivo):
     count_prev = 0
     branching_params = []
     for i in range(num_intervals):
+        # iterate through the bins
         inter = (orig, orig + bin_size)
         count = 0
+        # count the number of spikes in the bins
         for j in range(len(in_vivo)):
             if (in_vivo[j] < inter[1] and in_vivo[j] > inter[0]):
                 count += 1
         avalanches.append(count)
-
+        # compute the branching parameter
         if count_prev != 0:
             branching_params.append(count / count_prev)
         count_prev = count
@@ -39,25 +59,40 @@ def count_avalanches(bin_size, time_line, in_vivo):
         pbar.update()
     pbar.close()
     branching = np.mean(branching_params)
+    # remove the zero avalanches and compute the avalanches as the consecutive nonzero bins.
+    f = lambda x: x == 0
+    aval_proper = [i for k, g in groupby(avalanches, f) for i in (g if k else (sum(g),))]
 
-    return avalanches, branching
+    return aval_proper, branching
 
 
-bins = [2, 5, 10, 15, 20, 25, 50, 70, 110]
+bins = [1, 2, 4, 8, 16, 32, 64]
 avalanches_per_bin = {}
 branching_per_bin = {}
 for binn in bins:
+    # compute the avalanches per bin
     avalanches_per_bin[binn], branching_per_bin[binn] = count_avalanches(bin_size=binn, time_line=time_line,
                                                                          in_vivo=in_vivo)
 
 plt.figure(figsize=(11, 7))
 s1 = []
 for binn in bins:
+    # compute the frequency of single events f(s=1)
     s1.append(Counter(avalanches_per_bin[binn])[1])
 
     frequency = Counter(avalanches_per_bin[binn])
     frequency = dict(sorted(frequency.items()))
     values, counts = zip(*frequency.items())
+    # estimate the exponent of power-law(if any) and run statistical tests
+    # for power law distributions of the generated data
+    fit_alpha, fit_loc, fit_scale = stats.powerlaw.fit(counts, loc=0)
+    D, p = stats.kstest(counts, 'powerlaw', args=(fit_alpha, fit_loc, fit_scale))
+    if p > 0.05:
+        exponent = stats.powerlaw.fit(counts)[0]
+        print(f'The exponent of the power-law distribution is {exponent}')
+    print(f"for {binn} bins, the KS test statistic is {D}")  # Kolmogorov-Smirnov test statistic
+    print(f"for {binn} bins, the p-value is {p}")  # p-value of the test
+
     # Plotting the line graph
     plt.loglog(values, counts, marker='o', linestyle='-', label=f'bin size={binn}')
     # Adding labels and title
@@ -66,10 +101,10 @@ for binn in bins:
     plt.title('Frequency plot of in vivo data')
     plt.legend(loc='best')
 # Display the plot
-plt.plot(values, 1 / np.asarray(values), linestyle='--', label=r'power-law $\tau=1$')
 plt.legend()
 plt.show()
 
+# visualise the frequency of avalanches of size 1
 plt.loglog(bins, s1, marker='o', linestyle='-')
 plt.xlabel('Bin size(ms)')
 plt.grid(True)
@@ -77,6 +112,7 @@ plt.ylabel('f(s=1, bs)')
 plt.title('A different avalanche measure to assess criticality')
 plt.show()
 
+# visualise the branching parameter
 values_branch, counts_branch = zip(*branching_per_bin.items())
 plt.figure(figsize=(11, 6))
 plt.semilogx(values_branch, counts_branch, marker='o', linestyle='-', label=r'$\sigma^{*}$')
